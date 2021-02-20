7fa1a35bed0fa1ff577a2a0061a41738ff5892bb
Jonathyn Carter <jonathyncarter@gmail.com>
2021-02-20 10:43:03 -0500

    updates settings

--- theshoecollector/settings.py
+++ theshoecollector/settings.py
@@ -13,6 +13,10 @@ import os
 from decouple import config
 from pathlib import Path
 
+#from dotenv import load_dotenv, find_dotenv
+
+# load_dotenv(find_dotenv())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
